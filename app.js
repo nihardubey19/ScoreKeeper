2268c3003b7d7f5f0aed7a2e17d6d5c7f299959e
Nihar Dubey <nihardubey19@gmail.com>
2021-09-01 21:19:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playTo');
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore;
+let isgameOver = false;
+
+p1Button.addEventListener('click',()=>{
+    if(!isgameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            isgameOver=true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
+        }
+        p1Display.textContent=p1Score;
+    }
+});
+
+p2Button.addEventListener('click',()=>{
+    if(!isgameOver){
+        p2Score++;
+        if(p2Score == winningScore){
+            isgameOver=true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
+        }
+        p2Display.textContent=p2Score;    
+    }
+});
+
+resetButton.addEventListener('click',reset);
+
+winningScoreSelect.addEventListener('change',function(){
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+function reset(){
+    isgameOver=false;
+    p1Score=0;
+    p2Score=0;
+    p1Display.textContent=p1Score;
+    p2Display.textContent=p2Score;
+    p1Display.classList.remove('winner','loser');
+    p2Display.classList.remove('winner','loser');
+}
